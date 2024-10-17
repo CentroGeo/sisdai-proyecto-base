d38864050c8d728ffb99e0cc684f02262b0f2750
Pradel <crip.dadsig019@conahcyt.mx>
2024-10-17 07:27:14 -0600

    :wrench: feat: Se agregan esqueletos de vista de visualizaciones y vista de otras

---
+++ src/views/PaginaOtraVista.vue
@@ -0,0 +1,11 @@
+<script></script>
+
+<template>
+  <main
+    class="contenedor"
+    id="principal"
+  >
+    <h1>Otra vista</h1>
+  </main>
+</template>
+
---
+++ src/views/PaginaVisualizaciones.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+  <main
+    class="contenedor"
+    id="principal"
+  >
+    <h1>Visualizaciones</h1>
+  </main>
+</template>
